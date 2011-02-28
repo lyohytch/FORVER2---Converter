96c601f82e3be402532f191b1fce0a7bf330bf61
Alexey Kulyasov <Alexey.Kulyasov@teleca.com>
2011-02-28 20:08:11 +0300

    flushing tool

---
+++ flush_project.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
+buildDir = './build/'
+currDir = './'
+
+def removeMakeFiles(mkfDir):
+    if os.path.isdir(mkfDir):
+        if os.path.exists(mkfDir + '/' + 'Makefile'):     
+            os.remove(mkfDir + '/' + 'Makefile')
+            print 'Makefile from ' + mkfDir + ' removed.'
+       
+
+def removeBuildFolders(tmpDir):
+    if os.path.isdir(tmpDir):
+        if os.path.exists(tmpDir):
+            shutil.rmtree(tmpDir)
+            print tmpDir + ' deleted.'
+#MAIN
+if __name__ == '__main__':
+    print 'Flushing project stared.'
+    dirList = os.listdir(currDir)
+    buildDirList = os.listdir(buildDir)
+    removeMakeFiles(currDir)
+    for dirname in dirList:
+        removeMakeFiles(currDir + dirname)
+    
+    for dirname in buildDirList:
+        removeBuildFolders(buildDir + '/' + dirname)
+    
+    print 'Project successfully flushed.'
+            
+
+ 
+    
