9ccc26726dcaf4978a4a8608566e2905210248ae
Alexey Kulyasov <alexey.kulyasov@gmail.com>
2011-03-09 14:49:49 +0300

    update iview.h

--- presenter/iview.h
+++ presenter/iview.h
@@ -1,30 +1,66 @@
 #ifndef IVIEW_H
 #define IVIEW_H
 #include <QMainWindow>
+//! IView интерфейс для GUI
+/**
+  * Класс является интерфейсом для построения GUI. Здесь объявлены все сигналы от GUI и виртуальные функции,
+  * которые должны быть имплементированы в классе GUI. Подписывание на сигналы происходит в Presenter , GUI только
+  * посылает нужные сигналы и не нагружен лишней функциональностью
+  * \sa Presenter
+  */
 class IView: public QMainWindow
 {
     Q_OBJECT
 
 public:
     virtual ~IView() {}
-
+    /**
+      * Конструктор по-умолчанию
+      * \param parent указатель на родительский виджет. Необходим для инициализации QMainWindow
+      */
     IView (QWidget *parent) : QMainWindow(parent) {}
-
+    /**
+      * Указатели на объекты QModelDescribing
+      */
     QObject *model4, *modelP, *modelD, *modelJURA;
+    /**
+      * Массив объектов QModelDescribing
+      */
     QObject **models;
-
+    /**
+      * Массив объектов TreeViewModel
+      */
     QObject **trees;
-
+    /**
+      * Указатель на объект CorrelationModel
+      */
     QObject *corrModel;
 
-    //QueryModel QModelDescring->StringList
+    /**
+      * Указатель на объект querymodel
+      */
     QObject* queryModel;
+    /**
+      * Указатель на объект mssqlquery
+      */
     QObject* mssqlQuery;
-
-
+    /**
+      * Удалить какие-нибудь виджеты
+      */
     virtual void removeWidgets() = 0;
+    /**
+      * Добавить какие-нибудь виджеты
+      */
     virtual void addWidgets() = 0;
+    /**
+      * Обновить сообщение на форме
+      * \param txtMsg текстовое сообщение
+      */
     virtual void updateTextLabel(const QString& txtMsg) = 0;
+    /**
+      * Установить или нет английский язык
+      * \param check true - установить, false - иначе
+      */
     virtual void englishLanguageSet(bool check) = 0;
     virtual void russianLanguageSet(bool check) = 0;
 
