a0672a119847190c1094de76c56a023d9574da61
Alexey Kulyasov <alexey.kulyasov@gmail.com>
2011-06-12 15:18:27 +0400

    calculate total code size

--- getLOCinCPP.py
+++ getLOCinCPP.py
@@ -4,12 +4,16 @@ import os
 import sys
 
 searchExts = ['.c', '.cpp', '.h', '.hpp']
+total = 0
 
 def getLOC(arg, dirname, names):
     for fname in names:
         pathToFile = os.path.join(dirname, fname)   
         if os.path.splitext(pathToFile)[1] in searchExts:
-            print 'File:\t',fname,'\tLOC:\t',sum (1 for l in open(pathToFile, "r")),'\tFolder:\t',os.path.dirname(pathToFile)
+            global total
+            local = sum (1 for l in open(pathToFile, "r"))
+            total = total + local
+            print 'File:\t',fname,'\tLOC:\t',local,'\tFolder:\t',os.path.dirname(pathToFile)
 #MAIN
 if __name__ == '__main__':
     wrkDir = os.getcwd()
@@ -17,6 +21,8 @@ if __name__ == '__main__':
         if os.path.exists(sys.argv[1]) and os.path.isdir(sys.argv[1]):
             wrkDir = sys.argv[1] 
     os.path.walk( wrkDir, getLOC, None)
+    global total
+    print 'Total:', total
 
                             
                   
